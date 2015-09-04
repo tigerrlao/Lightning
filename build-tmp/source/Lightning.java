aa9cae4368539bbb8400df91f19556332759710e
tigerrlao <tigerrlao@gmail.com>
2015-09-04 08:37:00 -0700

    Lightning

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 PImage photo;
 int startX = 250;
 int startY = 0;
@@ -10,7 +26,7 @@ int lasers = 0;
 int dance = 0;
 boolean dancingMan = false;
 boolean disappear = false;
-void setup()
+public void setup()
 {
   size(500,500);
   background(0);
@@ -18,7 +34,7 @@ void setup()
   photo = loadImage("white-ghost.png");
 }
 
-void draw()
+public void draw()
 {
 	if(lighting == true)
 	{
@@ -30,7 +46,7 @@ void draw()
 	}
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	if(mouseButton == LEFT)
 	{
@@ -62,7 +78,7 @@ void mousePressed()
 	}
 
 }
-void fade()
+public void fade()
 {
 	noStroke();
 	fill(0,0,0,30);
@@ -77,7 +93,7 @@ void fade()
 	}
 }
 
-void stage()
+public void stage()
 {
 	time = time + 1;
 	while(lasers<15)
@@ -115,7 +131,7 @@ void stage()
 		}
 }
 
-void danceMan()
+public void danceMan()
 {
 	strokeWeight(5);
 	stroke(0,0,255);
@@ -146,7 +162,7 @@ void danceMan()
 	endShape(CLOSE);
 
 }
-void danceMan2()
+public void danceMan2()
 {
 	strokeWeight(5);
 	stroke(0,0,255);
@@ -187,4 +203,13 @@ void danceMan2()
 	vertex(360,210);
 	vertex(300,330);
 	endShape(CLOSE);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
